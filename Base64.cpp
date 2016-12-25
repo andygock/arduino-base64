2a6ad3a212703fac7c8d65e43282ee5e3137a8cd
Andy Gock <andy@agock.com>
2016-12-26 00:51:34 +1100

    Make compatible with ESP8266 boards

--- Base64.cpp
+++ Base64.cpp
@@ -1,5 +1,10 @@
 #include "Base64.h"
-#include <avr/pgmspace.h>
+#ifdef __AVR__
+	#include <avr/pgmspace.h>
+#elif defined(ESP8266)
+	#include <pgmspace.h>
+#endif
+
 const char PROGMEM b64_alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 		"abcdefghijklmnopqrstuvwxyz"
 		"0123456789+/";
